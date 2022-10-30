137f942ea24ffc982426634adaada5233765db40
pk393256 <pk393256@gmail.com>
2022-10-30 23:12:44 +0530

    javascript

--- DP-sum equals to k/index.js
+++ DP-sum equals to k/index.js
@@ -1,2 +1,24 @@
 //Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum. 
 
+function equalPartition(arr, n)
+    {
+        let totalSum=0
+        let targetSum
+        var ans=0
+        for(let i=0;i<n;i++){
+            totalSum+=arr[i]
+        }
+        if(totalSum%2!=0){return 0}
+        else{
+            targetSum=totalSum/2
+        }
+        check(0,targetSum)
+        if(ans==1){
+            return 1
+        }else{
+            return 0
+        }
+        
+        
+          
+    }
