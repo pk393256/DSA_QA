9b2468ebca80751284763154dcea85f4f7ba93bd
pk393256 <pk393256@gmail.com>
2022-11-03 23:06:40 +0530

    question

---
+++ Solve Age of empires/index.js
@@ -0,0 +1,16 @@
+
+/*
+You are playing the popular game of "Age of Empires". You are the king of the empire where you have 2N workers.
+
+All workers will be grouped in the association with size 2, so a total of N associations have to be formed.
+
+The building speed of the i-th worker is A[i].
+
+To make an association, you pick up 2 workers. Let the minimum building speed between both workers be x, then the association has the resultant building speed x.
+
+You have to print the maximum value possible of the sum of building speeds of N associations if you make the associations optimally.
+
+
+
+*/
+
