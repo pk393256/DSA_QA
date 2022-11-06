f36f362034e7e95c78c4e1d8dd7d066e118042dc
pk393256 <pk393256@gmail.com>
2022-11-06 13:20:06 +0530

    question

---
+++ Sort it out/index.js
@@ -0,0 +1,10 @@
+/*
+You are given a permutation consisting of n numbers 1, 2, ..., n (a permutation is an array in which each element from 1 to n occurs exactly once).
+
+You are allowed to perform the following operation any number of times:
+
+Choose a subarray (contiguous segment) of this permutation and arrange it any way you want.
+Note: You cannot choose the entire sequence at once.
+
+Your task is to calculate the minimum number of operations described above to sort the permutation a in ascending order.
+*/
