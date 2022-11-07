f05af5f4612b8d9ef82ef731942f55e84e9c0555
pk393256 <pk393256@gmail.com>
2022-11-07 23:10:58 +0530

    question

--- Flower Management/index.js
+++ Flower Management/index.js
@@ -1,1 +1,7 @@
 //level medium
+
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer arrayflowerbedcontaining0's and1's, where0means empty and1means not empty, and an integern, find out if ifnnew flowers can be planted in theflowerbedwithout violating the no-adjacent-flowers rule.
+*/
