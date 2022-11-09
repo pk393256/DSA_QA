2d0267d97f2ead70f300387b9b7f001eac5e1c13
pk393256 <pk393256@gmail.com>
2022-11-09 23:35:05 +0530

    question

---
+++ Nikhil and his Search/index.js
@@ -0,0 +1,12 @@
+//level mid
+/*
+Nikhil and his friend Sachin are on a quest to find the answer to Life.
+
+In order to complete it, they need to answer Q queries on an array A having N integers. The queries can be of the following two types:
+
+0 x: Find the number of numbers in A which are not less than x.
+
+1 x: Find the number of numbers in A which are greater than x.
+
+Help them complete the quest.
+*/
