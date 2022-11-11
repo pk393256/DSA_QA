3c63e1b3ffcf2736e8af4a898d37fe42dcf822f2
pk393256 <pk393256@gmail.com>
2022-11-11 23:48:30 +0530

    question

---
+++ Sum Exists or Not/index.js
@@ -0,0 +1,7 @@
+//level : med
+
+/*
+Given an array of non-negative integers, and a value 'sum', determine if there is a subset of the given set (array) with sum equal to given sum.
+
+If there is a subset whose sum is equal to the required sum then print "yes" else print "no" without quotes.
+*/
