fe3e821bb7e1a239a653e9b8598d23b006a0086a
pk393256 <pk393256@gmail.com>
2022-11-11 23:49:11 +0530

    answer

--- Sum Exists or Not/index.js
+++ Sum Exists or Not/index.js
@@ -5,3 +5,40 @@ Given an array of non-negative integers, and a value 'sum', determine if there i
 
 If there is a subset whose sum is equal to the required sum then print "yes" else print "no" without quotes.
 */
+function doit(input) {
+    let lines = input.split('\n')
+    let n = parseInt(lines[0])
+    let arr = lines[1].trim().split(' ').map(Number)
+    let sum = parseInt(lines[2])
+    
+    if (rec(arr, sum, 0, 0)) {
+        console.log("yes")
+    } else {
+        console.log("no")
+    }
+}
+
+function rec(arr, sum, i, cur_sum) {
+    if (cur_sum == sum) {
+        return true
+    }
+    
+    if (i >= arr.length) {
+        return false
+    }
+    
+    return rec(arr, sum, i + 1, cur_sum + arr[i]) || rec(arr, sum, i + 1, cur_sum)
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    
+    doit(read)
+});
