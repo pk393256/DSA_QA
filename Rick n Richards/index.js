34c5d0a72d7daadc126ee9370bd2dc6729e9ff6b
pk393256 <pk393256@gmail.com>
2022-11-12 23:54:50 +0530

    question

--- Rick n Richards/index.js
+++ Rick n Richards/index.js
@@ -1,1 +1,10 @@
 //level mid
+/*
+There is a contest of Competitive eating. Rick and Richard were participating in the game.
+
+N bananas are placed in a line. Rick starts to eat from left to right, and Richard from right to left. For each banana, its length is known.
+
+Rick eats with speed twice that of Richard. If a player starts to eat the banana, other player can't touch it. If both players reach the same banana simultaneously, Rick gets to eat that banana as he snatches it away from Richard.
+
+How many bananas each of the players will eat?
+*/
